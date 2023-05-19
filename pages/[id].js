a29d16a24b6c9187d45ff18c11fc762361979b75
root <root@localhost.localdomain>
2023-05-19 11:17:45 +0000

    Pagina blog modificada

--- pages/[id].js
+++ pages/[id].js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import Layout from '../../components/layout'
-import Header from '../../components/header'
+import Layout from '/../components/layout'
+import Header from '/../components/header'
 import Image from 'next/image'
 
 export default function Blog({post}) {
